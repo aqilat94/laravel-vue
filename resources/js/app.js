e253a52259c7ada19faf0ee842e6a55eec4d849e
aqilat94 <aqilat94@gmail.com>
2022-03-20 20:26:55 +0800

    register index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,16 @@ require('./bootstrap');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import router from './routes';
+import index from './index';
 
 window.Vue = require('vue').default;
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    components: {
+        'index': index
+    }
 });
