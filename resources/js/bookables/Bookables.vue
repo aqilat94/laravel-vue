3976696c8fbeb2c106c9af244e875119a44a77cd
aqilat94 <aqilat94@gmail.com>
2022-03-22 11:12:42 +0800

    Display bookbale item in bookable component

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <bookable-list-item item-title="Cheap Villa" item-content="Very cheap villa" :price="1000"></bookable-list-item>
+        <bookable-list-item item-title="Cheap Villa 2" item-content="Very cheap villa 2" :price="2000"></bookable-list-item>
+    </div>
+</template>
+
+<script>
+import BookableListItem from './BookableListItem'
+
+export default {
+    components: {
+        BookableListItem
+    }
+}
+</script>
