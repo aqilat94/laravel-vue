8d0e1f99d1c012cc7e063b364c25583308cf6122
aqilat94 <aqilat94@gmail.com>
2022-03-23 00:20:00 +0800

    add data and created

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <bookable-list-item item-title="Cheap Villa" item-content="Very cheap villa" :price="1000"></bookable-list-item>
-        <bookable-list-item item-title="Cheap Villa 2" item-content="Very cheap villa 2" :price="2000"></bookable-list-item>
+        <bookable-list-item :item-title="bookable1.title" :item-content="bookable1.content" :price="1000"></bookable-list-item>
+        <bookable-list-item :item-title="bookable2.title" :item-content="bookable2.content" :price="2000"></bookable-list-item>
     </div>
 </template>
 
@@ -11,6 +11,24 @@ import BookableListItem from './BookableListItem'
 export default {
     components: {
         BookableListItem
+    },
+    data() {
+        return {
+            bookable1: {
+                title: 'Cheap Villa !!!',
+                content: 'Very Cheap Villa'
+            },
+            bookable2: {
+                title: 'Cheap Villa 2',
+                content: 'Very Cheap Villa 2'
+            },
+        }
+    },
+    created() {
+        console.log('created');
+        console.log(this.bookable1);
+        console.log(this.bookable2);
     }
+
 }
 </script>
